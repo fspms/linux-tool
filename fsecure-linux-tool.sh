5abebbf875f162e78db7823035b09378657ef9d5
fspms <32630772+fspms@users.noreply.github.com>
2017-10-23 10:24:55 +0200

    int

--- fsecure-linux-tool.sh
+++ fsecure-linux-tool.sh
@@ -25,7 +25,7 @@ fi
 ##auto update## 
 
 #DIR="$( cd "$( dirname "$0" )" && pwd )"
-DIR=/opt/fspms
+DIR="/opt/fspms"
 configfile=$(cat $DIR/.git/config | grep "https://github.com/fspms/linux-tool")
 
 if [ ${#configfile} -gt "1" ]
